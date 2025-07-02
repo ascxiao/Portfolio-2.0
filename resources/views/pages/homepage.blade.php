badba528323b3caa33e5cb84a42055ddb9929b31
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2025-07-02 23:28:45 +0800

    Reinitialize

---
+++ resources/views/pages/homepage.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        @vite('resources/css/app.css')
+    </head>
+
+    <body>
+        <main class = "px-[107px] py-6">
+            <section class = "w-full min-h-screen">
+                <div class ="flex mt-[137px]">
+                    <div class = "text-3xl font medium text-black">welcome!</div>
+                </div>
+                <div class = "flex mt-[149px]">
+                    <div>
+                        <h1 class="text-8xl">PORTFOLIO '25</h1>
+                        <p class = "text-3xl pt-6 subheading">CARLOS MIGUEL C. VALDERRAMA</p>
+                        <div class="relative text-2xl mt-[15px]">
+                            <p class = "absolute transition-opacity duration-[1.5s] opacity-100">illustrator</p>
+                            <p class = "absolute transition-opacity duration-[1.5s] opacity-0">data scientist</p>
+                        </div>
+                        <div class="flex items-center max-w-[232px] max-h-[67px] mt-[138px] rounded-[32px] bg-green-900 p-6 text-white">
+                                <img class="size-[61px] ml-[12px]" src="/img/arrow-right.png" alt="Arrow Right Logo"/>
+                            <div class = "text-2xl ml-4">let's go?</div>
+                        </div>
+
+                        <div class = "flex size-[48px] mt-[85px] space-x-6">
+                            <img src="/img/linkedin.png" alt="LinkedIn"/>
+                            <img src="/img/github.png" alt="Github"/>
+                            <img src="/img/instagram.png" alt="Instagram"/>
+                            <img src="/img/gmail.png" alt="Gmail"/>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img/>
+                    </div>
+                </div>
+            </section>
+
+            <section class = "w-full min-h-screen">
+            <div>
+
+            </div>
+        </main>
+    </body>
+</html>
